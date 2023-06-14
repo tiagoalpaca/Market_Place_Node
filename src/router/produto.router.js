7afd87d5bc89e79da1c08c98bd17992b32953269
tiago avellar <tiagoalpaca@gmail.com>
2023-06-14 18:29:57 -0300

    Feat:Validação do Produto e da Categoria

--- src/router/produto.router.js
+++ src/router/produto.router.js
@@ -6,17 +6,19 @@ const produtoController = require("../controller/produto.controller");
 
 // importar o middleware para fazer as verificações
 const authMiddleware = require ("../middleware/auth.middleware");
+// importar o middleware para fazer as validações,normalmente as validações ocorrem no Post e no Put
+const {validaProduto} = require ("../middleware/validacao.middleware");
 
 // metodos get
 router.get("/find/:id",authMiddleware,produtoController.findProductByIdController);
 router.get("/findAll",authMiddleware,produtoController.findAllProductsController);
 
 // metodo Post
-router.post("/create",authMiddleware,produtoController.createProductController);
+router.post("/create",authMiddleware,validaProduto,produtoController.createProductController);
 router.post("/addCategoria/:id",authMiddleware,produtoController.addCategoriaProdutoController);
 
 // Metodo Put
-router.put("/update/:id",authMiddleware,produtoController.updateProductController);
+router.put("/update/:id",authMiddleware,validaProduto,produtoController.updateProductController);
 
 // Metodo Delete
 router.delete("/delete/:id",authMiddleware,produtoController.removeProductController);
